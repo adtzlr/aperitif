90d4ab572fc90d12510f53f3a404332aadde182f
Andreas D <a.dutzler@gmail.com>
2020-10-26 20:24:01 +0100

    initial commit
    add an input file reader to aperitif

---
+++ aperitif/reader.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created     on 2020-10-26 20:00
+Last Edited on 2020-10-26 20:00
+
+@author: adtzlr
+                           .__  __  .__  _____ 
+_____  ______   ___________|__|/  |_|__|/ ____\
+\__  \ \____ \_/ __ \_  __ \  \   __\  \   __\ 
+ / __ \|  |_> >  ___/|  | \/  ||  | |  ||  |   
+(____  /   __/ \___  >__|  |__||__| |__||__|   
+     \/|__|        \/                          
+
+
+APERITIF - Nonlinear Finite Elements Code for Structural Mechanics
+Copyright (C) 2020  Dutzler A.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from types import SimpleNamespace
+
+import pandas as pd
+import numpy as np
+
+from scipy.interpolate import interp1d
+
+import fem
+import geometry
+
+import readertools
+
+#%% Input
+def read(filename):
+    """
+    Read an input file and return modeldata.
+    
+    Parameters
+    ----------
+    filename : str
+        The full path and filename of the inputfile.
+        
+    Returns
+    -------
+    model : SimpleNamespace
+        The generated model data as struct.
+    """
+    
+    print('''
+                               .__  __  .__  _____ 
+    _____  ______   ___________|__|/  |_|__|/ ____\
+    \__  \ \____ \_/ __ \_  __ \  \   __\  \   __\ 
+     / __ \|  |_> >  ___/|  | \/  ||  | |  ||  |   
+    (____  /   __/ \___  >__|  |__||__| |__||__|   
+         \/|__|        \/                          
+    _______________________________________________________
+    Nonlinear Finite Elements Code for Structural Mechanics
+    
+    ''')
+    print('    Version:   2020.10')
+    print('    Author:    Dutzler A.')
+    
+    print('')
+    print('READ INPUT FILE')
+    print('===============\n')
+    
+    # read xlsx-inputfile to dataframe
+    df = pd.read_excel(filename,sheet_name=None)
+    
+    # get data of sheets from xlsx-file
+    elementdata    = df['Elements']
+    materialdata   = df['Materials']
+    tabledata      = df['Tables']
+    #tableptsdata   = df['Tablepoints']
+    forcedata      = df['Forces']
+    dofdata        = df['ControlledDOF']
+    bcdata         = df['BoundaryConditions']
+    lcdata         = df['Loadcases']
+    
+    # create structs for elements and materials
+    model = SimpleNamespace()
+    model.filename  = filename
+    print('Inputfile:', model.filename)
+
+    # create material, table, boundary, dof and loadcase library from inputfile
+    model.elements   = readertools.create_elements(elementdata)
+    model.materials  = readertools.create_materials(materialdata)
+    model.tables     = readertools.create_tables(tabledata)
+    model.boundaries = readertools.create_boundary_conditions(bcdata)
+    model.dof        = readertools.create_dofs(dofdata)
+    model.loadcases  = readertools.create_loadcases(lcdata)
+        
+    # get nodal coordinates
+    model.nodes = df['Nodes'].drop(columns=['Id']).values.astype(float)
+    
+    # global model data
+    model.nnodes, model.ndim = model.nodes.shape
+    model.nelements = len(model.elements.connectivities)
+    model.ndof = model.nnodes*model.ndim
+    
+    print('\n')
+    print('Global Model Data')
+    print('-----------------')
+    print('No. of Elements: %d' % model.nelements)
+    print('No. of Nodes:    %d' % model.nnodes)
+    print('No. of DOF:      %d' % model.ndof)
+    print('\n')
+          
+    # add 'None' Table for constant valued boundary Conditions
+    tbl0 = readertools.init_const_table()
+    
+    # add dummy tables for "None,Fixed,Constant"
+    model.tables['None'] = tbl0
+    model.tables['Fixed'] = tbl0
+    model.tables['Constant'] = tbl0
+
+    # create external displacements from inputfile
+    # get array with displacement boundary condition label for each dof
+    model._ext_displacements_b = np.array(dofdata.drop(columns=['Id']).values, dtype=object)
+    model._ext_displacements_0 = np.zeros_like(model.nodes)
+    # get array with nodal force boundary label for each dof
+    model._ext_forces_b = np.array(forcedata.drop(columns=['Id']).values, dtype=object)
+    model._ext_forces_0 = np.zeros_like(model.nodes)
+    
+    # loop over boundary conditions (displacement and force) 
+    # and replace labels with table multiplier values
+    for blabel, bc in model.boundaries.items():
+        model._ext_displacements_0[model._ext_displacements_b == blabel] = bc.value
+        model._ext_forces_0[model._ext_forces_b == blabel] = bc.value
+    
+    # replace all non-prescribed dof's with NaN multiplier vaLue
+    model._ext_displacements_0[model.dof.active] = np.nan
+    
+    # save external forces and disp. to data
+    model.ext_forces = readertools.ext_forces
+    model.ext_displacements = readertools.ext_displacements
+    
+    model.elements.v0  = np.zeros(model.nelements)
+    model.elements.Tai = np.zeros(model.nelements,dtype=object)
+    model.elements.Kai = np.zeros(model.nelements,dtype=object)
+    model.elements.Kbj = np.zeros(model.nelements,dtype=object)
+    for a,(etype,conn) in enumerate(zip(model.elements.types,
+                                        model.elements.connectivities)):
+        model.elements.v0[a]  = geometry.volume(model.nodes[conn])
+        model.elements.Tai[a] = fem.element[etype.lower()].indices_ai(conn)
+        aibj = fem.element[etype.lower()].indices_aibj(conn)
+        model.elements.Kai[a] = aibj[0]
+        model.elements.Kbj[a] = aibj[1]
+        
+    if any([etype.endswith('p') for etype in model.elements.types]):
+           print('\nThree-Field-Variational Principle:')
+           print(  '----------------------------------')
+           print('Π_int = ∫ψ dV + ∫mP(J^m-θ) dV\n')
+        
+    model.elements.Tai = np.concatenate(model.elements.Tai)
+    model.elements.Kai = np.concatenate(model.elements.Kai)
+    model.elements.Kbj = np.concatenate(model.elements.Kbj)
+
+    return model
---
+++ aperitif/readertools.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created     on 2020-10-26 20:00
+Last Edited on 2020-10-26 20:00
+
+@author: adtzlr
+                           .__  __  .__  _____ 
+_____  ______   ___________|__|/  |_|__|/ ____\
+\__  \ \____ \_/ __ \_  __ \  \   __\  \   __\ 
+ / __ \|  |_> >  ___/|  | \/  ||  | |  ||  |   
+(____  /   __/ \___  >__|  |__||__| |__||__|   
+     \/|__|        \/                          
+
+
+APERITIF - Nonlinear Finite Elements Code for Structural Mechanics
+Copyright (C) 2020  Dutzler A.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+def convert_material_parameters(mtype,mparam,mat):
+    if mtype.lower() == 'neo-hooke':
+        mat.parameters.mu = mparam[1]
+        mat.parameters.K = mparam[0]
+    if mtype.lower() == 'mooney-rivlin':
+        mat.parameters.C10 = mparam[1]
+        mat.parameters.C01 = mparam[2]
+        mat.parameters.K = mparam[0]
+    if mtype.lower() == 'invariants':
+        mat.parameters.K   = mparam[0]
+        mat.parameters.C10 = mparam[1]
+        mat.parameters.C01 = mparam[2]
+        mat.parameters.C11 = mparam[3]
+        mat.parameters.C20 = mparam[4]
+        mat.parameters.C30 = mparam[5]
+    if mtype.lower() == 'multi-k' or 'multi-k-test':
+        mat.parameters.n = int(mparam[1])
+        mat.parameters.mu = mparam[2:mat.parameters.n+2]
+        mat.parameters.k = mparam[mat.parameters.n+2:2*mat.parameters.n+2]
+        mat.parameters.K = mparam[0]
+    if mtype.lower() == 'svk':
+        mat.parameters.n = int(mparam[1])
+        mat.parameters.mu   = mparam[0*mat.parameters.n+2:1*mat.parameters.n+2]
+        mat.parameters.lmbd = mparam[1*mat.parameters.n+2:2*mat.parameters.n+2]
+        mat.parameters.k    = mparam[2*mat.parameters.n+2:3*mat.parameters.n+2]
+        mat.parameters.a    = mparam[3*mat.parameters.n+2:4*mat.parameters.n+2]
+        mat.parameters.K    = mparam[0]
+    if mtype.lower() == 'svk-simple':
+        E = mparam[1]
+        nu = mparam[2]
+        mat.parameters.mu   = E/(2*(1+nu))
+        mat.parameters.lmbd = E*nu/((1+nu)*(1-2*nu))
+        
+    return mat
+    
+def create_elements(elementdata):
+    elements  = SimpleNamespace()
+    
+    # store element type "Quad4" as lowercase string "quad4"
+    # store material labels (case sensitive)
+    # store element connectivity matrix
+    elements.types          = [etype.lower() for etype in elementdata.Type.values]
+    elements.materiallabels = elementdata.Material.values
+    elements.connectivities = elementdata.drop(columns=['Type', 'Material', 'Id']).values.astype(int)
+    return elements
+    
+def create_materials(materialdata):
+    # create material library from inputfile
+    # init an empty dictionary, loop over materials in inputfile
+    materials = {}
+    for mlabel,mtype,mparam in zip(materialdata.Label.values, 
+                                   materialdata.Type.values, 
+                                   materialdata.drop(columns=['Label', 
+                                                              'Type']).values):
+
+        # create material as struct and save it in the materials-dictionary
+        mat = SimpleNamespace()
+        mat.label = mlabel
+        mat.type = mtype
+        mat.parameters = SimpleNamespace()
+        mat.parameters.type  = mtype.lower()
+        
+        mat = convert_material_parameters(mtype,mparam,mat)
+        materials[mlabel] = mat
+    return materials
+    
+def create_tables(tabledata):
+    # create table library from inputfile
+    # init an empty dictionary, loop over tables in inputfile
+    tables = {}
+    
+    for tid,tlabel,ttype in zip(tabledata.Id,tabledata.Label,tabledata.Type):
+
+        if not np.isnan(tid):
+            # create table as struct and save it in the tbl-dictionary
+            tbl = SimpleNamespace()
+            tbl.id = tid
+            tbl.label = tlabel
+            tbl.type = ttype.lower()
+            x = tabledata['x'+str(int(tid))].values
+            f = tabledata['f'+str(int(tid))].values
+            tbl.points = (np.vstack((x,f)).T)[~np.isnan(x)]
+            tbl.eval = interp1d(x[~np.isnan(x)],f[~np.isnan(x)])
+            
+            tables[tbl.label] = tbl
+    return tables
+    
+def create_boundary_conditions(bcdata):
+    # create boundary conditions library from inputfile
+    # init an empty dictionary, loop over boundaries in inputfile
+    boundaries = {}
+    for blabel,bvalue,btable in zip(bcdata.Label,bcdata.Value,bcdata.Table):
+
+        # create bc as struct and save it in the bc-dictionary
+        bc = SimpleNamespace()
+        bc.label = blabel
+        bc.value = bvalue
+        bc.table = btable
+        
+        boundaries[blabel] = bc
+    return boundaries
+    
+def create_loadcases(lcdata):
+    # create loadcase library from inputfile
+    # init an empty list, loop over loadcases in inputfile
+    loadcases = []
+    _t0 = 0.0
+    for llabel,ldur,lincs,liter,ltol in zip(lcdata.Label,
+                                            lcdata.Duration,
+                                            lcdata.Increments,
+                                            lcdata.Iterations,
+                                            lcdata.Tolerance):
+
+        # create lc as struct and save it in the lc-dictionary
+        lcase = SimpleNamespace()
+        lcase.label = llabel
+        lcase.duration = ldur
+        lcase.nincs = lincs
+        lcase.nmax = liter
+        lcase.gtol = ltol
+        lcase.start = _t0
+        
+        _t0 += lcase.duration
+        
+        loadcases.append(lcase)
+    return loadcases
+    
+def create_dofs(dofdata):
+    # create active (=free) / inactive (=prescribed, controlled) dof mask
+    dof = SimpleNamespace()
+    dof.inactive = dofdata.drop(columns=['Id']).values.astype(bool)
+    dof.active = ~model.dof.inactive
+    return dof
+    
+def init_const_table():
+    tbl0 = SimpleNamespace() 
+    tbl0.id = -1
+    tbl0.label = 'None'
+    tbl0.type = 'time'
+    x = np.array([0])
+    f = np.array([0])
+    tbl0.points = np.vstack((x,f)).T
+    tbl0.eval = lambda x: 1
+    return tbl0
+    
+# create time dependent function for external displacements
+def ext_displacements(t):
+    ed = model._ext_displacements_0.copy()
+    # evaluate boundary conditions at time t and save values
+    for blabel, bc in model.boundaries.items():
+        tbl_value = model.tables[bc.table].eval(t)
+        value = ed[model._ext_displacements_b==blabel]
+        ed[model._ext_displacements_b==blabel] = value*tbl_value
+    return ed
+
+def ext_forces(t):
+    ef = model._ext_forces_0.copy()
+    # evaluate boundary conditions at time t and save values
+    for blabel, bc in model.boundaries.items():
+        tbl_value = model.tables[bc.table].eval(t)
+        value = ef[model._ext_forces_b==blabel]
+        ef[model._ext_forces_b==blabel] = value*tbl_value
+    return ef
