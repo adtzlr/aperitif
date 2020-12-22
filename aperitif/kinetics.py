2e2e5b2858d3698d7d74c3a3c05da716ecb5cd9a
Andreas D <a.dutzler@gmail.com>
2020-12-22 15:39:06 +0100

    Update kinetics.py

--- aperitif/kinetics.py
+++ aperitif/kinetics.py
@@ -34,7 +34,7 @@ import mathlib
 from mathlib import deviator as dev
 import geometry
 
-from umats_modified import umat
+import constitution
 
 from types import SimpleNamespace
 
@@ -55,52 +55,33 @@ def kinematics(x0,u,v0,element):
     dhdx = dhdr@drdx
     dudx = u.T@dhdx
     
+    def extend(F):
+        'Extend deformation gradient to shape (3,3).'
+        F3D = np.eye(3)
+        n,m = F.shape
+        F3D[:n,:m] = F
+        return F3D
+    
     # init kinematics struct and evalutate jacobian Jr, 
     # deformation gradient F as well as J=det(F) at integration points
     # F has dimensions (n,n)!
     kin      = SimpleNamespace()
     kin.dhdx = dhdr@drdx
     kin.Jr   = np.array([det(dxdr_ip) for dxdr_ip in dxdr])
-    kin.F    = np.array([inv(I-dudx_ip) for dudx_ip in dudx])
+    kin.F    = np.array([inv(extend(I-dudx_ip)) for dudx_ip in dudx])
     kin.J    = np.array([det(F) for F in kin.F])
     
-    if element.shape.unimodular:
-        # total potential energy (internal force contribution)
-        # Π_int = ∫ψ dV + ∫mP(J^m-θ) dV
-        
-        # evaluate isochoric/unimodular part of deformation gradient
-        # naming convention is "unimodular part of deformation gradient": Fu
-        # Fu always has dimensions (3,3) - even in 2D analysis
-        def function_Fu(F,J,ndim):
-            F3D = mathlib.I[3].copy()
-            n,m = F.shape
-            F3D[:n,:m] = F
-            Fu = J**(-1/3)*F3D
-            #Fu = abs(J)**(2/3)/J*F3D
-            return Fu
-            
-        kin.Fu = np.array([function_Fu(F,J,ndim) for F,J in zip(kin.F,kin.J)])
-        kin.bu = np.array([tdot(Fu,Fu.T,1) for Fu in kin.Fu])
-        
-        # exponent on volume ratio for interpolation
-        kin.m = 1
-        
-        # get element volume ratio
-        kin.v = geometry.volume(x0+u)
-        kin.Jm = kin.v/v0
-        
-        # get modified deformation gradient
-        # and modified left cauchy-green deformation tensors
-        kin.Fm = kin.Jm**(1/3)*kin.Fu
-        kin.bm = kin.Jm**(2/3)*kin.bu
-        
-        # element-based mean shape function derivative
-        kin.M = 1/v0 * np.sum(
-                np.array([dhdx*Jr*w
-                          for dhdx,Jr,w in zip(kin.dhdx,
-                                               kin.Jr,
-                                               element.gauss.weights)]),
-                              axis=0)
+    # get element volume ratio
+    kin.v = geometry.volume(x0+u)
+    kin.Jm = kin.v/v0
+    
+    # element-based mean shape function derivative
+    kin.M = 1/v0 * np.sum(
+            np.array([dhdx*Jr*w
+                      for dhdx,Jr,w in zip(kin.dhdx,
+                                           kin.Jr,
+                                           element.gauss.weights)]),
+                          axis=0)
 
     return kin
 
@@ -118,86 +99,48 @@ def stiffness_force(u,x0,v0,kinematics,element,material):
     Jm = kinematics.Jm
     
     # TODO
-    state_vars = None
+    state_vars_old = None
     
-    sigma          = np.zeros((element.npgauss,3,3))
+    #sigma          = np.zeros((element.npgauss,6))
     internal_force = np.zeros((element.nnodes,element.ndim))
     
     # Evaluate pre-integrated part of Element Stiffness matrix
-    stiffness  = np.zeros((element.nnodes,element.ndim,
-                           element.ndim,  element.nnodes))
+    M = kinematics.M
+    stiffness  = 2*dpdJ*np.tensordot(M,M.T,0)
+                           
+    U    = constitution.U(   Jm,material,full_output=False)
+    p    = constitution.dUdJ(Jm,material,full_output=False)
+    dpdJ = constitution.dUdJ(Jm,material,full_output=False)
     
-    Tm = []
-    C4m = []
-    for ip, (J,Fu,bu) in enumerate(zip(
-            kinematics.J,
-            kinematics.Fu,
-            kinematics.bu)):
-        
-        tm,c4m,state_vars = umat(Fu,bu,state_vars,material)
-        Tm.append(tm)
-        C4m.append(c4m)
-        
-    p = 1/kinematics.v*np.array([tdot(tm,I,2)/3/J*Jr*w for tm,J,Jr,w in
-                                 zip(Tm,
-                                     kinematics.J,
-                                     kinematics.Jr,
-                                     element.gauss.weights)])
+    II  = np.einsum('ij,kl',I,I)
+    IIt = np.einsum('il,jk',I,I)
+    
+    s_ = []
 
-    # loop over integration points
-    for ip, (s,J,Fm,bm,Fu,bu,dhdx,Jr,w) in enumerate(zip(
-            sigma,
+    for ip, (J,F,dhdx,Jr,w) in enumerate(zip(
             kinematics.J,
-            kinematics.Fm,
-            kinematics.bm,
-            kinematics.Fu,
-            kinematics.bu,
+            kinematics.F,
             kinematics.dhdx,
             kinematics.Jr,
             element.gauss.weights)):
         
-        # quantities at integration points
-        # --------------------------------
-        # tu  ... unimodular part of kirchhoff stress
-        # c4u ... unimodular part of spatial rank-4 elasticity tensor
-        # J   ... determinant of deformation gradient
-        # Jr  ... determinant of jacobian deformed-->natural configuration
-        # w   ... integration point weight
+        ψ, state_vars = constitution.ψ(      F,material,state_vars_old)
+        P             = constitution.dψdF(   F,material,state_vars,full_output=False)
+        A             = constitution.d2ψdFdF(F,material,state_vars,full_output=False)
         
-        tm,c4m,state_vars = umat(Fu,bu,state_vars,material)
-
-        tr_tm = tdot(tm,I,2)
-
-        s = dev(tm)/J+p*I
-
-        sigma[ip] = s
-
-        # internal force vector
+        sdev = np.einsum('jJ,iJ',F,P)/J
+        adev = np.einsum('jJ,lL,iJkL',F,F,A)/J
+        
+        s = sdev+p*I
+        s_.append(mathlib.tovoigt(s))
+        
+        a = adev+p*(II-IIt)
+        
         internal_force += tdot(dhdx,s[:ndim,:ndim],1)*Jr*w
         
-        # helper tensor products of `tm` and `I`
-        tI = tdot(tm,I,0)
-        It = tI.transpose([2,3,0,1])
-        tIt = (tI.transpose([0,2,1,3]) +
-               tI.transpose([0,3,1,2]) )/2
-
-        # deviatoric part of constitutive elasticity matrix
-        c4_dev = 2/3 * (tr_tm*IIt - (tI+It) + tr_tm/3*II) / J
-        c4_hyd = J/Jm * 2/3 * (It+tdot(I,tdot(I,c4m,2),0)+c4m-1/3*tI+tdot(c4m,II,4)*II)
-
-        if np.any(c4m):
-            # calculate only if necessary
-            c4_dev += tdot( tdot(p4, c4m, 2), p4, 2)/J
-
-        # geometric stiffness matrix
-        c4_geo = (tIt.transpose([1,0,3,2])/2-p*IIt/2
-               + p*(II-3/2*IIt))
-                  
-        # absolute elasticity matrix
-        a4 = c4_dev + c4_hyd + c4_geo
-        a4t = a4[:ndim,:ndim,:ndim,:ndim].transpose([1,0,2,3])
-        stiffness += tdot( tdot(dhdx,a4t,1), dhdx.T, 1) * Jr*w
+        at = a[:ndim,:ndim,:ndim,:ndim].transpose([1,0,2,3])
+        stiffness += tdot( tdot(dhdx,at,1), dhdx.T, 1) * Jr*w
         
-    stress = np.array([mathlib.tovoigt(s) for s in sigma])
+    stress = np.array([mathlib.tovoigt(s) for s in s_])
         
     return internal_force, stiffness.transpose([0,1,3,2]), stress
